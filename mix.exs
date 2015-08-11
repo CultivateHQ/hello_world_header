85ca00693ee0125ee04df5ba10c8f7f1456f6c32
Mark Connell <mark@markconnell.co.uk>
2015-08-11 15:53:07 +0100

    metadata

--- mix.exs
+++ mix.exs
@@ -3,6 +3,8 @@ defmodule HelloWorldHeader.Mixfile do
 
   def project do
     [app: :hello_world_header,
+     description: "Demo plug used for tutorial purposes.",
+     package: package,
      version: "0.0.1",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
@@ -17,5 +19,12 @@ defmodule HelloWorldHeader.Mixfile do
   defp deps do
     [{:cowboy, "~> 1.0"},
      {:plug, "~> 0.14 or ~> 1.0"}]
+  end
+
+  defp package do
+    [contributors: ["Mark Connell",],
+     licenses: ["MIT"],
+     links: %{github: "https://github.com/cultivatehq/hello_world_header"},
+     files: ~w(lib mix.exs README.md)]
   end
 end
